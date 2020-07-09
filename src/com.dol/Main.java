70be390d4f03c146b467302eb24af782fe6873cf
Senushkinev <senushkinev@yande.ru>
2020-07-09 04:15:38 +0300

    Добавил метод для, того чтобы юра мог порисовать графики.

--- src/com.dol/Main.java
+++ src/com.dol/Main.java
@@ -1,9 +1,22 @@
 package com.dol;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         Dijkstra dij = new Dijkstra();
-        dij.dijkstra(new Coordinates(550, 850));
+
+        Coordinates from = new Coordinates(600, 400);
+        Coordinates to = new Coordinates(605, 405);
+        /* Крч, Юра, если ты адово горишь желаниием порисовать графики, то, как работает это дерьмо
+           После того как заюзал dijkstra сюда передается стартовая точка
+           Можно использовать getWayFromCoordinates, сюда передаешь конечную точку, получаешь список координат, если идти по которым получишь кратчайщий путь
+           ЧТОБЫ ТЕБЕ НЕ КОМПИЛИТЬ КУЧУ РАЗ, Я ИХ ПОКА РАЗДЕЛИЛ, ТЕБЕ НЕ ОБЯЗАТЕЛЬНО ЮЗАТЬ ВСЕ ВРЕМЯ dijkstra МОЖЕШЬ ОДИН РАЗ ЗАДАТЬ НАЧАЛО, А ПОТОМ ПОЛУЧАТЬ КУЧУ ПУТЕЙ В РАЗНЫЕ ТОЧКЧИ
+         */
+
+        dij.dijkstra(from);
+        LinkedList<Coordinates> LL = dij.getWayFromCoordinates(to);
+
     }
 }
